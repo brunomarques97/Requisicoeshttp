513a1470a83e5a2975f65c5e055bcf7b8059cc32
brunomarques97 <116980642+brunomarques97@users.noreply.github.com>
2023-01-24 11:09:55 -0300

    Resgatando dados da api

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
 
+import{useState,useEffect} from "react";
+
+const url="http://localhost:3000/products";
+
 function App() {
+  const [produtos,setprodutos]=useState([])
+
+  useEffect(()=>{
+    async function fecthdata(){
+      const res =await fetch(url);
+      const data =await res.json();
+      setprodutos(data);
+    }
+    fecthdata();
+  },[]);
+
+
   return (
     <div className="App">
         <h1>Lista de produtos</h1>
+        <ul>
+          {produtos.map((produto)=>(
+            <li key={produto.id}>
+              {produto.name}
+            </li>
+          ))}
+        </ul>
     </div>
   );
 }
